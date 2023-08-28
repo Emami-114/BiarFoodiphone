2b4fd2a3d204c2e5e00da33d6153a7284887a32b
Emami.114 <emami.114@outlook.de>
2023-08-28 16:49:00 +0200

    Initial Commit

---
+++ BiarFoodiphone/BiarFoodiphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiarFoodiphoneApp.swift
+//  BiarFoodiphone
+//
+//  Created by Ecc on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BiarFoodiphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
